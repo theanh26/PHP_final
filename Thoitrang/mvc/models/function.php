a37e29ce3b4365cbc0730bba7d5a6cc0c6e5d70a
ThanhNguyen-DA <nguyen2303thanh@email.com>
2023-08-12 17:22:11 +0700

    admin

--- Thoitrang/mvc/models/function.php
+++ Thoitrang/mvc/models/function.php
@@ -57,5 +57,113 @@ class handle extends database
         return $gan;
     }
 
+    public function insert_user($email, $phone, $name, $adress, $pass, $avatar)
+    {
+        $sql = "INSERT INTO `users`(`email`, `phone`, `name`, `adress`, `pass`, `avatar`) VALUES
+         ('$email','$phone','$name','$adress','$pass','$avatar')";
+        $this->execute($sql);
+    }
+
+    public function insert_cate($name, $parent)
+    {
+        $sql = "INSERT INTO `cates`(`name_cate`, `parent_cate`) VALUES ('$name','$parent')";
+        $this->execute($sql);
+    }
+
+    public function insert_product($title, $content, $thongtin, $image, $quantity, $price, $sale, $special, $cate_id)
+    {
+        date_default_timezone_set('Asia/Ho_Chi_Minh');
+        $date = date('Y-m-d H:i:s');
+        $sql = "INSERT INTO `products`( `title_pro`, `content_pro`, `thongtin`, `image_pro`, `quantity_pro`,
+         `price_pro`, `sale_pro`, `date_pro`, `special_pro`, `cate_id`) VALUES ('$title','$content','$thongtin','$image','$quantity',
+         '$price','$sale','$date','$special','$cate_id')";
+        $this->execute($sql);
+    }
+
+    public function insert_comment($content, $id_pro, $id_user, $parent)
+    {
+        date_default_timezone_set('Asia/Ho_Chi_Minh');
+        $date = date('Y-m-d H:i:s');
+        $sql = "INSERT INTO `comments`( `content_cm`, `product_id`, `user_id`, `date`, `parent_cm`) 
+        VALUES ('$content','$id_pro','$id_user','$date','$parent')";
+        $this->execute($sql);
+    }
+
+    public function insert_loves($id_pro, $id_user)
+    {
+        $sql = "INSERT INTO `loves`(`pro_id`, `user_id`) VALUES ('$id_pro','$id_user')";
+        $this->execute($sql);
+    }
+
+    public function insert_banner($image, $title, $id_pro)
+    {
+        $sql = "INSERT INTO `banners`(`image_banner`, `title_banner`, `id_product`) VALUES ('$image','$title','$id_pro')";
+        $this->execute($sql);
+    }
+    public function insert_album($image, $id_pro)
+    {
+        $sql = "INSERT INTO `albums`( `image_album`, `id_product`) VALUES ('$image','$id_pro')";
+        $this->execute($sql);
+    }
+    public function insert_info($email, $logo, $phone, $adress, $content)
+    {
+        $sql = "INSERT INTO `info`(`email_info`, `logo`, `phone_info`, `adress_info`, `thongtin`) VALUES ('$email','$logo','$phone','$adress','$content')";
+        $this->execute($sql);
+    }
+
+    public function update_cate($name_cate, $parent, $id_cate)
+    {
+        $sql = "UPDATE `cates` SET `name_cate`='$name_cate',`parent_cate`='$parent' WHERE `id_cate` = $id_cate";
+        $this->execute($sql);
+    }
+
+    public function update_product($id_name, $id, $title, $content, $thongtin, $image, $quantity, $price, $sale, $special, $hiden, $cate_id)
+    {
+        $sql = "UPDATE `products` SET `title_pro`='$title',`content_pro`='$content',`thongtin`='$thongtin',`image_pro`='$image',
+        `quantity_pro`='$quantity',`price_pro`='$price',`sale_pro`='$sale',`special_pro`='$special',`hiden_pro`='$hiden',
+        `cate_id`='$cate_id' WHERE $id_name = $id";
+        $this->execute($sql);
+    }
+
+    public function update_view($view, $id_pro)
+    {
+        $sql = "UPDATE `products` SET `view_pro`='$view' WHERE `id_pro` = $id_pro";
+        $this->execute($sql);
+    }
+
+    public function update_user($phone, $name, $adress, $avatar, $id)
+    {
+        $sql = "UPDATE `users` SET `phone`='$phone',`name`='$name',`adress`='$adress',`avatar`='$avatar' WHERE `id_user` = $id";
+        $this->execute($sql);
+    }
+    // public function update_ma_code($id, $code)
+    // {
+    //     $sql = "UPDATE `users` SET `code_mk`='$code' WHERE `id_user` = $id";
+    //     $this->execute($sql);
+    // }
+    // public function update_mk($pass, $id, $code_mk = '', $code = '')
+    // {
+    //     $sql = "UPDATE `users` SET `pass`='$pass' WHERE `id_user` = $id";
+    //     if (!empty($code_mk) && !empty($code)) {
+    //         $sql .= " AND `$code_mk` = '$code'";
+    //     }
+    //     $this->execute($sql);
+    // }
+
+    public function update_nhanvien($email, $phone, $name, $address, $pass, $avatar, $role, $look, $id)
+    {
+        $sql = "UPDATE `users` SET `email`='$email',`phone`='$phone',`name`='$name',`adress`='$address',`pass`='$pass',`avatar`='$avatar',`role`='$role',`look`='$look' WHERE `id_user` = $id";
+        $this->execute($sql);
+    }
+
+    public function check_loves($id_pro, $id_user)
+    {
+        $sql = "SELECT * FROM `loves` 
+        JOIN users ON users.id_user = loves.user_id
+        JOIN products ON products.id_pro = loves.pro_id
+        WHERE users.id_user = $id_user AND products.id_pro = $id_pro ";
+        $gan = $this->select($sql);
+        return $gan;
+    }
 }
 ?>
---
+++ Thoitrang/mvc/views/admin/user.php
@@ -0,0 +1,68 @@
+<main>
+
+    <h2 class="dash-title">Overview</h2>
+    <div class="table-responsive">
+        <table class="table table-xuly ">
+            <thead>
+                <tr>
+                    <th>
+                        <input type="checkbox" name="" id="checkall" hidden>
+                        <button class="btn btn-danger">
+                            <label for="checkall" class="select">chọn tất</label>
+                            <label for="checkall" class="unselect" style="display: none;">bỏ chọn</label>
+                        </button>
+                    </th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Tên</th>
+                    <th>Địa chỉ</th>
+                    <th>mật khẩu</th>
+                    <th>Ảnh</th>
+                    <th>Quyền</th>
+                    <th>Kích hoạt</th>
+                    <th>Mã</th>
+                    <th>Yêu thích</th>
+                    <th>Sửa</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php //var_dump($post->get_val('users'));
+                // if(isset($_POST['delete_user'])){
+                //     var_dump($_POST);
+                // }
+                if (!empty($post->get_val('users'))) {
+                    foreach ($post->get_val('users') as $val) : extract($val); ?>
+                        <tr>
+                            <form action="" method="post">
+                                <td><input type="checkbox" name="checkbox[]" id="check" value="<?= $id_user ?>"></td>
+                                <td><?= $email ?></td>
+                                <td><?= $phone ?></td>
+                                <td><?= $name ?></td>
+                                <td><?= $adress ?></td>
+                                <td><?= $pass ?></td>
+                                <td><img src="../public/img/<?= $avatar ?>" width="100px" height="100px" alt=""></td>
+                                <td><?php if ($role < 1) {
+                                        echo "người dùng";
+                                    } else if ($role > 1) {
+                                        echo "nhân viên";
+                                    } else {
+                                        echo "admin";
+                                    }  ?></td>
+                                <td><?php echo $look = $look < 1 ? "bình thường" : "bị khóa"; ?></td>
+                                <td><?= $code_mk ?></td>
+                                <td><a href="<?= CONTROLLERS_ADMIN ?>?action=dsyeuthich&id_user=<?= $id_user ?>" class="btn btn-danger">thích</a></td>
+                                <td><a href="<?= CONTROLLERS_ADMIN ?>?action=sua_user&id_user=<?= $id_user ?>" class="btn btn-warning">Sửa</a></td>
+                        </tr>
+                <?php endforeach;
+                } ?>
+            </tbody>
+
+        </table>
+        <tr>
+            <td><input onclick="return confirm('bạn muốn xóa theo lua chon');" class="btn btn-danger" type="submit" name="delete_user" value="xóa click" id="checkall"></td>
+        </tr>
+        </form>
+    </div>
+
+
+</main>
