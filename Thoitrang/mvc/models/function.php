3f362b01a310f85a255bece5b849c0ce5e5b2d7e
ThanhNguyen-DA <nguyen2303thanh@email.com>
2023-08-14 16:38:45 +0700

    1408

--- Thoitrang/mvc/models/function.php
+++ Thoitrang/mvc/models/function.php
@@ -165,5 +165,192 @@ class handle extends database
         $gan = $this->select($sql);
         return $gan;
     }
+
+    //----------------------đơn hàng và chi tiết đơn hàng----------------------------
+    public function select_order($action = "")
+    {
+        $sql = "SELECT * FROM `orders`";
+        if($action != ""){
+            $sql .= " WHERE `action` = $action ORDER BY `date_order` DESC";
+        }else{
+            $sql .=" ORDER BY `date_order` DESC";
+        }
+        $gan = $this->select($sql);
+        return $gan;
+    }
+    public function select_order_date()
+    {
+        $sql = "SELECT * FROM `orders` WHERE `action` = 2 ORDER BY `date_order` DESC ";
+        $gan = $this->select($sql);
+        return $gan;
+    }
+    public function insert_order($tong, $phone, $email, $adress, $name, $content)
+    {
+        date_default_timezone_set('Asia/Ho_Chi_Minh');
+        $date = date('Y-m-d H:i:s');
+        $sql = "INSERT INTO `orders`(`total_order`, `phone_order`, `email_order`, `adress_order`, `name_order`, `content_order`, `date_order`) VALUES ('$tong','$phone','$email','$adress','$name','$content','$date')";
+        $this->execute($sql);
+    }
+    public function insert_order_detail($quantity, $order_id, $pro_id)
+    {
+        $sql = "INSERT INTO `order_detail`( `quantity_detail`, `order_id`, `product_id`) VALUES ('$quantity','$order_id','$pro_id')";
+        $this->execute($sql);
+    }
+    public function update_quantity($quantity, $id)
+    {
+        $sql = "UPDATE `products` SET `quantity_pro`='$quantity' WHERE `id_pro` = $id";
+        $this->execute($sql);
+    }
+    public function id_order()
+    {
+        $sql = "SELECT MAX(`id_order`) as 'id' FROM `orders` WHERE 1";
+        $gan = $this->select($sql);
+        return $gan;
+    }
+    public function insert_histrory($id_user, $id_order)
+    {
+        $sql = "INSERT INTO `histrorys`(`id_user`, `id_order`) VALUES ('$id_user','$id_order')";
+        $this->execute($sql);
+    }
+    public function get_histrory($id_user)
+    {
+        $sql = "SELECT * FROM `histrorys` JOIN users ON users.id_user = histrorys.id_user
+        JOIN orders ON orders.id_order = histrorys.id_order WHERE users.id_user = $id_user";
+        $gan = $this->select($sql);
+        return $gan;
+    }
+    public function gui_dh($action, $id)
+    {
+        $sql = "UPDATE `orders` SET `action`='$action' WHERE `id_order` = $id";
+        $this->execute($sql);
+    }
+    public function select_love_join()
+    {
+        $sql = "SELECT*FROM `loves` 
+        JOIN products ON products.id_pro = loves.pro_id  
+        JOIN users ON users.id_user = loves.user_id ";
+        $gan = $this->select($sql);
+        return $gan;
+    }
+    public function insert_news($title, $content, $image, $author)
+    {
+        date_default_timezone_set('Asia/Ho_Chi_Minh');
+        $day = date('Y-m-d H:i:s');
+        $sql = "INSERT INTO `news`( `title`, `content`, `image`, `date`, `author`) 
+        VALUES ('$title','$content','$image','$day','$author')";
+        $this->execute($sql);
+    }
+    public function update_news($id, $title, $content, $image, $author)
+    {
+        $sql = "UPDATE `news` SET `title`='$title',`content`='$content',`image`='$image'
+        ,`author`='$author' WHERE `id_news` = $id";
+        $this->execute($sql);
+    }
+    //-----------------------------------thong ke cm---------------------
+    public function thongke_cm()
+    {
+        $sql = "SELECT products.id_pro AS 'id_pro',products.title_pro AS 'ten_pro',COUNT(comments.id_cm) AS 'so_cm',MAX(comments.date) AS 'max_bl',MIN(comments.date) AS 'min_bl' 
+        FROM `comments` 
+        JOIN users ON comments.user_id = users.id_user
+        JOIN products ON products.id_pro = comments.product_id 
+        GROUP BY products.id_pro";
+        $gan = $this->select($sql);
+        return $gan;
+    }
+    public function thongke_cm_chitiet($id)
+    {
+        $sql = "SELECT comments.id_cm AS 'id',users.name AS 'name_bl', comments.content_cm AS 'content', comments.date AS 'date'  FROM `comments` 
+        JOIN users ON comments.user_id = users.id_user
+        JOIN products ON products.id_pro = comments.product_id 
+        WHERE products.id_pro = $id";
+        $gan = $this->select($sql);
+        return $gan;
+    }
+    //-----------------------------------hien thi don hang---------------------
+    public function thongke_order_detail($id)
+    {
+        $sql = "SELECT * FROM `order_detail` 
+        JOIN products ON products.id_pro = order_detail.product_id
+        WHERE order_detail.`order_id` = $id";
+        $gan = $this->select($sql);
+        return $gan;
+    }
+
+    //------------------------------------so hang ban duoc-----------------------
+    public function thongke_buy_pro()
+    {
+        $sql = "SELECT products.title_pro AS 'tensp',SUM(order_detail.quantity_detail) AS 'soct_theosp',
+        products.price_pro AS 'gia' ,products.sale_pro AS 'sale'
+        FROM `orders` 
+        JOIN order_detail ON orders.id_order = order_detail.order_id
+        JOIN products ON products.id_pro = order_detail.product_id WHERE orders.`action` = 2 GROUP BY products.id_pro";
+        $gan = $this->select($sql);
+        return $gan;
+    }
+    public function banchay()
+    {
+        $sql = "SELECT products.title_pro AS 'name',SUM(order_detail.quantity_detail) AS 'banchay' ,products.image_pro as 'image'
+        FROM `orders` 
+        JOIN order_detail ON orders.id_order = order_detail.order_id
+        JOIN products ON products.id_pro = order_detail.product_id WHERE orders.`action` = 2 GROUP BY products.id_pro
+        ORDER BY order_detail.quantity_detail DESC LIMIT 1";
+        $gan = $this->select($sql);
+        return $gan;
+    }
+    public function view_thongke()
+    {
+        $sql = "SELECT sum(`view_pro`) AS 'view' FROM `products` WHERE 1";
+        $gan = $this->select($sql);
+        return $gan;
+    }
+    public function ngay_thongke($thang)
+    {
+        $sql = "SELECT * FROM orders WHERE action = 2 AND MONTH(date_order)='$thang'";
+        $gan = $this->select($sql);
+        return $gan;
+    }
+
+    //-----------------------------------loc------------------------------
+    public function loc_sp($start = 0, $end = 0, $id_cate = '', $search = '', $one = '', $two = '')
+    {
+        //ham loc nay bat buoc phai co tham so truyen day du tham so cuoi k can cung dc
+        $sql = "SELECT * FROM `products` JOIN cates ON cates.id_cate = products.cate_id
+        WHERE 1";
+        if ($id_cate != '') {
+            $sql .= " AND products.cate_id = $id_cate";
+        }
+        if ($start != 0 && $end != 0) {
+            $sql .= " AND products.price_pro BETWEEN $start AND $end";
+        } else if ($start != 0) {
+            $sql .= " AND products.price_pro > $start";
+        }
+        if ($search != '') {
+            $sql .= " AND products.title_pro LIKE '%$search%'";
+        }
+        if ($one != '' && $two != '') {
+            $sql .= " LIMIT $one,$two";
+        }
+        $gan = $this->select($sql);
+        return $gan;
+    }
 }
+
+$error = array();
+$post = new xuly();
+
+//--------------------------loc------------------------------------
+function phantrang($tongsp, $sp, $page)
+{
+
+    $start = ($page - 1) * $sp; //0-4
+    $sum_page = ceil($tongsp / $sp);
+    $mang = array(
+        'start' => $start,
+        'tong_page' => $sum_page,
+        'sp' => $sp
+    );
+
+    return $mang;
+}
+//var_dump($post->loc_sp(0,0,null));
 ?>
